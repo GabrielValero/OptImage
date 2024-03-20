18fefae5b8446a76183e03373b93abf67941bc6d
Gabriel Valero <63328736+GabrielValero@users.noreply.github.com>
2024-03-20 18:51:21 -0400

    feat: add constants file

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,6 @@
+const dimensions = {
+    navigationIcon: 30
+}
 const colors = {
     mainColor: "#D213E3",
     backgroundColor: "#242529",
@@ -5,4 +8,4 @@ const colors = {
     gray: "#34353B"
 }
 
-export default colors
+export {dimensions, colors}
